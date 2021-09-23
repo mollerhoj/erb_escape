65f8923229711ed8376097d6c1efd1e4e480364e
mollerhoj <mollerhoj3@gmail.com>
2021-09-23 16:11:15 +0200

    init

---
+++ lib/erb_escape.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "erb_escape/version"
+
+module ErbEscape
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/erb_escape/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ErbEscape
+  VERSION = "0.1.0"
+end
